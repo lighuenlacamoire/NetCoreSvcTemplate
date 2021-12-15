3e9701bc44102fbb9e47427675ed63c1f6655f68
Lighuen Lacamoire <llacamoire@grupomercel.com>
2021-12-15 15:22:56 -0300

    prueba de nuget

--- obj/Debug/net6.0/GaliciaSegurosSvcTemplate.AssemblyInfo.cs
+++ obj/Debug/net6.0/GaliciaSegurosSvcTemplate.AssemblyInfo.cs
@@ -14,13 +14,14 @@ using System.Reflection;
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("385d83c4-c838-4824-ab4d-b1ecae8b738f")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banco Galicia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2022")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Template para los microservicios de Galicia Seguros")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GaliciaSegurosSvcTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GaliciaSegurosSvcTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/threenine/api-template")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/lighuenlacamoire/NetCoreSvcTemplate")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
